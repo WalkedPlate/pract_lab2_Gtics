9470e54984f7ba51452724f77386f8c2de484ae0
WalkedPlate <144744565+WalkedPlate@users.noreply.github.com>
2024-04-10 21:31:37 -0500

    Initial commit

---
+++ src/main/java/com/example/pract_lab2_gtics/PractLab2GticsApplication.java
@@ -0,0 +1,13 @@
+package com.example.pract_lab2_gtics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PractLab2GticsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PractLab2GticsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/pract_lab2_gtics/PractLab2GticsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pract_lab2_gtics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PractLab2GticsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
